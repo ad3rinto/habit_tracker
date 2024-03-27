4460738a65c1ba5ca64076773d95d0a965c936e6
ad3rinto <aad3rinto@gmail.com>
2024-03-27 13:55:39 +0000

    refactored local version

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import requests
 from datetime import datetime
 
-# load_dotenv()
-TOKEN = os.environ.get['PIXELA_TOKEN']
-USERNAME = os.environ.get["USERNAME"]
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
+USERNAME = os.environ.get("USERNAME")
 GRAPH_ID = 'graph1'
 # weblink https://pixe.la/v1/users/aad3rinto/graphs/graph1.html
 # USED TO CREATE CREDENTIALS
-
+# print(TOKEN, USERNAME)
 pixela_endpoint = 'https://pixe.la/v1/users'
-user_params = {
-    'token': TOKEN,
-    'username': USERNAME,
-    'agreeTermsOfService': 'yes',
-    'notMinor': 'yes',
-}
+# user_params = {
+#     'token': TOKEN,
+#     'username': USERNAME,
+#     'agreeTermsOfService': 'yes',
+#     'notMinor': 'yes',
+# }
 
-response = requests.post(pixela_endpoint, user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
 # CREATE A GRAPH
 # graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
@@ -30,21 +30,23 @@ print(response.text)
 #     'unit': 'Km',
 #     'color': 'ajisai'
 # }
-# headers = {
-#     'X-USER-TOKEN': TOKEN
-# }
+#
+headers = {
+    'X-USER-TOKEN': TOKEN
+}
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
 # posting a value to the graph
 # today = datetime(year=2021, month=10, day=13)
-#print(today.strftime('%Y%m%d'))
+today = datetime.today()
+print(today.strftime('%Y%m%d'))
+#
+graph_post_pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
+post_pixel_params = {
+    'date': today.strftime('%Y%m%d'),
+    'quantity': '1500'
+}
 
-# graph_post_pixel_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
-# post_pixel_params = {
-#     'date': today.strftime('%Y%m%d'),
-#     'quantity': '500'
-# }
-
-# response = requests.post(url=graph_post_pixel_endpoint, json=post_pixel_params, headers=headers)
-# print(response.text)
+response = requests.post(url=graph_post_pixel_endpoint, json=post_pixel_params, headers=headers)
+print(response.text)
